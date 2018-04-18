538ab646c19994f85f5e72f57f25deb74b9c7c0d
Nick Kreeger <nick.kreeger@gmail.com>
2018-04-18 15:37:13 -0700

    Initial commit.

---
+++ src/queue.ts
@@ -0,0 +1,27 @@
+
+export class UniqueQueue<Value extends any> {
+  private size: number;
+  private keys: Set<Value>;
+  private items: Value[];
+
+  constructor(size = 0) {
+    this.size = size;
+    this.keys = new Set();
+    this.items = [];
+  }
+
+  push(value: Value) {
+    if (!this.keys.has(value)) {
+      this.keys.add(value);
+      this.items.unshift(value);
+      if (this.size > 0 && this.items.length > this.size) {
+        const key = this.items.pop();
+        this.keys.delete(key);
+      }
+    }
+  }
+
+  values(): Value[] {
+    return this.items;
+  }
+}
---
+++ src/test.ts
@@ -0,0 +1,35 @@
+import {UniqueQueue} from '.';
+
+describe('unique queue', () => {
+  it('returns FIFO', () => {
+    const queue = new UniqueQueue<number>();
+    queue.push(1);
+    queue.push(2);
+    queue.push(3);
+    expect(queue.values()).toEqual([3, 2, 1]);
+  });
+  it('pushes old objects if value is supplied', () => {
+    const queue = new UniqueQueue<number>(5);
+    for (let i = 0; i < 10; i++) {
+      queue.push(i);
+    }
+    expect(queue.values()).toEqual([9, 8, 7, 6, 5]);
+  });
+  it('should handle unique values', () => {
+    const queue = new UniqueQueue<number>();
+    queue.push(1);
+    queue.push(2);
+    queue.push(2);
+    queue.push(2);
+    expect(queue.values()).toEqual([2, 1]);
+  });
+  it('should handle unique values with size', () => {
+    const queue = new UniqueQueue<number>(3);
+    queue.push(1);
+    queue.push(2);
+    for (let i = 0; i < 3; i++) {
+      queue.push(i);
+    }
+    expect(queue.values()).toEqual([0, 2, 1]);
+  });
+});
